ab052f38ac2359525f63c678f21ce277f5d539c7
asadsidd16 <asadsidd22@outlook.com>
2025-05-22 12:56:29 -0700

    navigation setup

--- App.tsx
+++ App.tsx
@@ -4,7 +4,7 @@ import { StyleSheet, Text, View } from "react-native";
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>It works again reaaly!</Text>
+      <Text>It works!</Text>
       <StatusBar style="auto" />
     </View>
   );
---
+++ src/navigation/AppNavigator.tsx
@@ -0,0 +1,9 @@
+import React from "react";
+import { NavigationContainer } from "@react-navigation/native";
+import { createNativeStackNavigator } from "@react-navigation/native-stack";
+
+const Stack = createNativeStackNavigator();
+
+export default function App() {
+  return <NavigationContainer>{/*Screens here */}</NavigationContainer>;
+}
