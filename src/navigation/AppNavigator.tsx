7a769d6fd6f058811da5592e9e7b379a2f6dc1f9
asadsidd16 <asadsidd22@outlook.com>
2025-05-23 14:19:35 -0700

    removing what I dont need for the challenge

--- src/navigation/AppNavigator.tsx
+++
@@ -1,9 +0,0 @@
-import React from "react";
-import { NavigationContainer } from "@react-navigation/native";
-import { createNativeStackNavigator } from "@react-navigation/native-stack";
-
-const Stack = createNativeStackNavigator();
-
-export default function App() {
-  return <NavigationContainer>{/*Screens here */}</NavigationContainer>;
-}
